f9abb6d6f51e90d29ba070eb134bde57f39f4367
Ilix-studio <ilishjyoti17@gmail.com>
2022-03-31 17:31:23 +0530

    set up1

---
+++ server.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req,res) => res.status(200).send("Server Connnected"));
+
+
+app.listen(PORT, () => console.log(`listening to http://localhost:${5000}`));
